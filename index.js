11740252e2ec3466cab919a83a8ce0606559dbb8
Motty-har <harligmotty@gmail.com>
2023-04-24 11:36:20 -0400

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(drivers, string){
+    console.log(drivers)
+    console.log(string)
+    const newDrivers = drivers.filter(function (name){
+        return name.toUpperCase() === string.toUpperCase()
+    })
+    return newDrivers
+}
+
+function fuzzyMatch(drivers, string){
+    console.log("string", string)
+    const newDrivers2 = drivers.filter(function(name){
+        console.log("name", name)
+        return name.startsWith(string) 
+    }) 
+    return newDrivers2
+}
+
+
+function matchName(drivers, string){
+    console.log("drivers", drivers)
+    const newDrivers3 = drivers.filter(function(driver){
+       return driver.name === string
+    })
+    return newDrivers3 
+}
